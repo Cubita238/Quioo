9e5db50bcae1a90a9e808ffad69c469566f87c27
Tobilash <carmen.albarran.1932@gmail.com>
2024-08-20 09:03:56 -0600

    iniciado

---
+++ settings.js
@@ -0,0 +1,17 @@
+global.prefa = ['','!','.',',','🐤','🗿']
+
+global.owner = ['6285141149257']
+global.botname = 'アフマド-xBot'
+global.namaowner = 'Herzz'
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.sticker1 = "Herzz"
+global.sticker2 = "🌜"
+global.apikey = '_'
+global.capikey = '_'
+global.domain = '_'
+global.eggsnya = '_'
+global.location = '_'
+global.adana = '-'
+global.aqris = '-'
+global.aovo = '-'
+global.agopay = '-'
